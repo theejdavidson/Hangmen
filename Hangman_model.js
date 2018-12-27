a44774ef212bf67f53f88b1815989c0610c7d7e5
Ethan Davidson <theejdavidson@gmail.com>
2018-12-26 20:57:53 -0600

    set up beginning for hangman typescript and generated javascript for model

---
+++ Hangman_model.js
@@ -0,0 +1,35 @@
+var message = "Hello Player";
+console.log(message);
+var wordList = new Array("awkward", "bagpipes", "banjo", "croquet", "crypt", "dwarves", "fervid");
+var inputChar = "a";
+inputChar = inputChar.toLowerCase();
+var pick = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(pick);
+var incorrectGuess = 0;
+var blankArray = new Array();
+function displayAsBlanks(pick, blankArray) {
+    var i;
+    for (i = 0; i < pick.length; i++) {
+        blankArray.push("_");
+    }
+    return blankArray;
+}
+console.log(displayAsBlanks(pick, blankArray));
+function compareInput(pick, inputChar, blankArray, incorrectGuess) {
+    var indices = [];
+    var index = pick.indexOf(inputChar);
+    while (index >= 0) {
+        indices.push(index);
+        index = pick.indexOf(inputChar, index + 1);
+    }
+    console.log(indices);
+    if (indices.length == 0) {
+        console.log("incorrect guess!");
+        incorrectGuess++;
+    }
+    else {
+        console.log("correct guess with " + indices.length + " hits!");
+    }
+    return blankArray;
+}
+console.log(compareInput(pick, inputChar, blankArray, incorrectGuess));
---
+++ Hangman_model.ts
@@ -0,0 +1,44 @@
+var message:string ="Hello Player"
+console.log(message)
+
+var wordList:string[] = new Array("awkward", "bagpipes", "banjo", "croquet", "crypt", "dwarves", "fervid")
+
+var inputChar:string = "a"
+inputChar= inputChar.toLowerCase()
+
+var pick = wordList[Math.floor(Math.random()*wordList.length)];
+console.log(pick)
+
+var incorrectGuess:number = 0;
+
+var blankArray:string[] = new Array()
+
+function displayAsBlanks(pick:string,blankArray:string[]):string[] {
+    var i:number;
+    for(i=0; i<pick.length;i++){
+        blankArray.push("_")
+    }return blankArray
+}
+console.log(displayAsBlanks(pick,blankArray))
+
+function compareInput(pick:string, inputChar:string, blankArray:string[], incorrectGuess:number):string[] {
+
+    var indices:Array<number> = [];
+    var index:number = pick.indexOf(inputChar);
+    while (index >=0) {
+        indices.push(index);
+        index = pick.indexOf(inputChar, index + 1);
+    }
+
+    console.log(indices)
+
+    if(indices.length == 0)
+    {
+        console.log("incorrect guess!")
+        incorrectGuess++;
+    } else {
+        console.log(`correct guess with ${indices.length} hits!`)
+        
+    }return blankArray
+}
+console.log(compareInput(pick, inputChar, blankArray, incorrectGuess))
